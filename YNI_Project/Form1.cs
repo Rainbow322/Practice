86dce9624c0e4fc7b95fc031b7bd7e7704a89024
Rainbow322 <74511886+Rainbow322@users.noreply.github.com>
2020-11-28 14:24:00 +0300

    Add files via upload

---
+++ YNI_Project/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YNI_Project
+{
+    public partial class Form1 : Form
+    {
+        Game game;
+        public Form1()
+        {
+            InitializeComponent();
+            game = new Game(4);
+            Start_Game();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt32(((Button)sender).Tag);
+            game.Shift(position);
+            Restart(); 
+            if (game.checkNumbers())
+            {
+                MessageBox.Show("Вы выиграли!");
+                this.Close();
+            }
+        }
+
+        private Button button (int position)
+        {
+            switch (position)
+            {
+                case 0: return button0;
+                case 1: return button1;
+                case 2: return button2;
+                case 3: return button3;
+                case 4: return button4;
+                case 5: return button5;
+                case 6: return button6;
+                case 7: return button7;
+                case 8: return button8;
+                case 9: return button9;
+                case 10: return button10;
+                case 11: return button11;
+                case 12: return button12;
+                case 13: return button13;
+                case 14: return button14;
+                case 15: return button15;
+                default: return null;
+            }
+        }
+
+        private void menu_start_Click(object sender, EventArgs e)
+        {
+            Start_Game();
+        }
+
+        private void Start_Game()
+        {
+            game.Start();
+            for(int j = 0; j < 150; j++)
+            {
+                game.ShiftShuffle();
+            }
+            Restart();
+        }
+
+        private void Restart()
+        {
+            for ( int position = 0; position < 16; position++)
+            {
+                int num = game.getNumber(position);
+                button(position).Text = num.ToString();
+                button(position).Visible = (num > 0); 
+            }
+        }
+    }
+}
---
+++ YNI_Project/Game.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YNI_Project
+{
+    class Game
+    {
+        int size;
+        int[,] map;
+        int space_x, space_y;
+        static Random rand = new Random();
+
+        public Game(int size)
+        {
+            if (size < 2) size = 2;
+            if (size > 5) size = 5;
+            this.size = size;
+            map = new int[size, size];
+        }
+
+        public void Start()
+        {
+            for (int x = 0; x < size; x++)
+            {
+                for (int y = 0; y < size; y++)
+                {
+                    map[x, y] = coordToPos(x, y) + 1;
+                }
+            }
+            space_x = size - 1;
+            space_y = size - 1;
+            map[space_x, space_y] = 0;
+        }
+
+        public void Shift(int position)
+        {
+            int x, y;
+            posToCoord(position, out x, out y);
+            if (Math.Abs(space_x - x) + Math.Abs(space_y - y) != 1)
+                return;
+            map[space_x, space_y] = map[x, y];
+            map[x, y] = 0;
+            space_x = x;
+            space_y = y; 
+        }
+
+        public void ShiftShuffle()
+        {
+            Shift(rand.Next(0, size * size));
+        }
+
+        public bool checkNumbers()
+        {
+            for (int x = 0; x < size; x++)
+            {
+                for (int y = 0; y < size; y++)
+                {
+                    if (!(x == size - 1 && y == size - 1))
+                        if (map[x, y] != coordToPos(x, y) + 1)
+                        return false;
+                }
+            }
+            return true;
+        }
+        public int getNumber(int position)
+        {
+            int x, y;
+            posToCoord(position, out x, out y);
+            if (x < 0 || x >= size) return 0;
+            if (y < 0 || y >= size) return 0;
+            return map[x, y];
+        }
+
+        private int coordToPos(int x, int y)
+        {
+            return y * size + x;
+
+        }
+
+        private void posToCoord (int position, out int x, out int y)
+        {
+            x = position % size;
+            y = position / size;
+        }
+    }
+}
